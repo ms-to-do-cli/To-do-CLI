8b2dbaade5fd8514467e2dc91978f8bdc6579961
Lander Van laer <lander.vanlaer1@gmail.com>
2022-05-18 14:43:37 +0200

    add [INTERACTIVE] tests for add Task

--- test/helpers/interactive-prompt.ts
+++ test/helpers/interactive-prompt.ts
@@ -5,7 +5,7 @@
  * @param endWithExit whether it has to add the `exit` command at the end
  * @returns A callback function that returns the responses in order
  */
-export const interactivePrompt = (responses: Record<string, string | number>[], endWithExit = true): () => Record<string, string | number> => {
+export const interactivePrompt = (responses: Record<string, string | number | boolean>[], endWithExit = true): () => Record<string, string | number | boolean> => {
     if (endWithExit)
         responses.push({ commandName: 'exit' });
 
