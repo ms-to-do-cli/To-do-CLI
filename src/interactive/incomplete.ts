2435817d2e55a930d58631e1c5f9a4b13ddfbd52
Lander Van laer <lander.vanlaer1@gmail.com>
2022-05-31 00:22:32 +0200

    add filter to [INTERACTIVE] (in)complete

--- src/interactive/incomplete.ts
+++ src/interactive/incomplete.ts
@@ -32,7 +32,11 @@ async function incomplete(this: I): Promise<void> {
     if (!taskList)
         throw new Error(`Can not find TaskList with name ${listName}`);
 
-    const tasks = await taskList.getTasks();
+    const tasks = (await taskList.getTasks()).filter(t => t.status !== 'notStarted');
+
+    if (tasks.length === 0)
+        throw new Error('No Tasks found that are completed yet');
+
     const taskNames = tasks.map(task => task.title);
 
     const { taskName }: { taskName: string } = await Interactive.prompt([{
